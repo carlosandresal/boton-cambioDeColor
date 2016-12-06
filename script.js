964ce9da3fafb5ce2634cd1043ee80e3c469355e
Carlos Andres Alzate <un.artbliss@gmail.com>
2016-12-06 20:13:22 -0300

    globalizando la funcion

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-var background = ["#F44336", "#E91E63", "#9C27B0", "#673AB7", "#3F51B5", "#2196F3", "#03A9F4", "#00BCD4", "#009688", "#4CAF50", "#8BC34A", "#CDDC39", "#FFEB3B", "#FFC107", "#FF9800", "#FF5722", "#795548", "#9E9E9E", "#607D8B", "#000000", "#FFFFFF"];
+//var background = ["#F44336", "#E91E63", "#9C27B0", "#673AB7", "#3F51B5", "#2196F3", "#03A9F4", "#00BCD4", "#009688", "#4CAF50", "#8BC34A", "#CDDC39", "#FFEB3B", "#FFC107", "#FF9800", "#FF5722", "#795548", "#9E9E9E", "#607D8B", "#000000", "#FFFFFF"];
 
 var btn = document.getElementById('bg');
 var body = document.getElementById('body');
 
-function randomColor() {
-    var random = Math.floor(Math.random() * background.length);
-    var color = background[random];
+function randomColor(bckgArray) {
+    var random = Math.floor(Math.random() * bckgArray.length);
+    var color = bckgArray[random];
     body.style.backgroundColor = color;
 }
 
 
 window.onload = function load() {
-    randomColor();
+    var backgroundIni = ["#009688", "#4CAF50", "#8BC34A", "#CDDC39", "#FFEB3B", "#FFC107", "#FF9800", "#FF5722", "#795548", "#9E9E9E", "#607D8B", "#000000", "#FFFFFF"];
+    randomColor(backgroundIni);
 }
 
 function changeBackground() {
+    var backgroundBtn = ["#009688", "#4CAF50", "#8BC34A", "#CDDC39", "#FFEB3B", "#FFC107", "#FF9800", "#FF5722", "#795548", "#9E9E9E", "#607D8B", "#000000", "#FFFFFF"];
     btn.addEventListener("click", function () {
-        randomColor();
+        randomColor(backgroundBtn);
     });
 }
 changeBackground();
-
